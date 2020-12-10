3b08007bb4f030eb21b38c385f205bef622793ce
Criatian Mejia <darkmejia643@gmail.com>
2020-12-09 19:01:05 -0600

    clase 4, agregue comentarios

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const authRouter =  require('./routes/auth');
 const server = express();
 
 
-server.use("/auth", authRouter);
+server.use("/auth", authRouter); // auth router es el metodo que manda el contenido de lo que se quiere hacer
 
 server.listen(8080);
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,13 @@
+
 const express = require('express');
-const controller = require('../controllers/auth');
-const auth = express.Router();
+const controller = require('../controllers/auth');//En este momento esta dentro de views/auth, por lo que se necesita salir y entrar a la carpeta correspondiente
 
-auth.get('/', controller.renderAuth);
 
-module.exports = authRouter;
+const authRouter = express.Router()
+
+authRouter.get('/', controller.renderAuth);// le pide mandar el archivo html que en este caso es auth.js en controllers.
+//2. renderAuth hace referencia al metodo exports.renderAuth de auth.js en controllers
+
+
+
+module.exports = authRouter
